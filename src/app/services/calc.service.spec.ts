8156272fd92982fa8d2fc08ffc2cab186a8e90cf
Ravenser <oleg.ilarionov@knu.ua>
2021-06-08 12:30:17 +0300

    without tests

---
+++ src/app/services/calc.service.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CalcService } from './calc.service';
+
+describe('CalcService', () => {
+  let service: CalcService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CalcService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  fit ("check service", ()=>{
+    
+  })
+
+});
